a7806d0878e3e6f295c127654c22dafa274cf55e
SanyokN <al.nikishin2000@gmail.com>
2024-06-25 15:21:40 +0200

    hw10

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
-from functools import wraps
-from typing import Callable
+import sqlite3
+from pprint import pprint
 
-data = [20, "hi", 3.14]
-
-
-def decorator(filter_type: str):
-    def decorator_inner(func: Callable):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            if isinstance(result, list):
-                if filter_type == 'str':
-                    result = [item for item in result if not isinstance(item, str)]
-                elif filter_type == 'int':
-                    result = [item for item in result if not isinstance(item, int)]
-                elif filter_type == 'float':
-                    result = [item for item in result if not isinstance(item, float)]
-            return result
-        return wrapper
-    return decorator_inner
-
-
-@decorator('str')
-def get_data_str() -> list:
-    return data
-
-
-@decorator('int')
-def get_data_int() -> list:
-    return data
-
-
-@decorator('float')
-def get_data_float() -> list:
-    return data
-
-
-@decorator('please delete nothing')
-def get_data() -> list:
-    return data
-
-
-print(get_data_str())
-print(get_data_int())
-print(get_data_float())
-print(get_data())
+with sqlite3.connect('book_store.sqlite3') as connection:
+    cursor = connection.cursor()
+    query = """
+        CREATE TABLE IF NOT EXISTS authors(
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            authors_name TEXT NOT NULL,
+            year_of_birth INTEGER NOT NULL
+        );
+    """
+    cursor.execute(query)
+    another_table = """
+        CREATE TABLE IF NOT EXISTS books(
+	        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	        author_id INTEGER NOT NULL,
+            price FLOAT CHECK (price > 0),
+            description TEXT,
+            FOREIGN KEY (author_id) REFERENCES authors(id)
+        );             
+    """
+    insert_query = """
+        INSERT INTO authors (authors_name, year_of_birth)
+        VALUES (?, ?)
+    """
+    cursor.execute(insert_query, ["Adam Mickiewicz", 1798])
+    authors = [
+        ("William Shakespeare", 1564),
+        ("Agatha Christie", 1890),
+        ("Barbara Cartland", 1901),
+        ("Julia Child", 1912)
+    ]
+    cursor.executemany(insert_query, authors)
+    read_query = """
+        SELECT description
+        FROM books
+        WHERE (description LIKE "%Рецепт%")
+    """
+    result = cursor.execute(read_query)
+    # pprint(result.fetchall(), indent=5)
+    read_query2 = """
+        SELECT description
+        FROM books
+        LIMIT 2
+        OFFSET 1
+    """
+    result2 = cursor.execute(read_query2)
+    pprint(result2.fetchall(), indent=1)
